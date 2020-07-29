9d8962acc4d1dd46975f7ea53a234ccb0d798fad
postonoh <postonoh@att.net>
2020-07-29 08:16:53 -0400

    ShelbyBlog example

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+const API_URL = "http://localhost:3000/api/posts";
+const API_BASE_URL = "http://localhost:3000/";
+
+window.onload = () => {
+    getPosts();
+}
+
+const getPosts = () => {
+    fetch(API_URL, {
+         method: 'GET'
+    }).then((response) =>{
+
+        return response.json();
+    }).then((data) => {
+        buildPosts(data);
+
+    });
+}
+
+const buildPosts = (blogPosts) => {
+
+}
